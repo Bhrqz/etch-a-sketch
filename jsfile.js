c27a0968591ceaca37adef44a52472e00d305e6e
Bhrqz <josebchrist@hotmail.com>
2020-04-12 17:19:23 -0500

    like a pen

--- jsfile.js
+++ jsfile.js
@@ -22,40 +22,41 @@ const title = document.getElementById("#title");
 title.style.textAlign = "center"
 title.style.marginTop= 30;
 
+
 //div container
 const container = document.querySelector("#container")
 container.style.display= "grid"
 container.style.backgroundColor="gray"
 container.style.justifyContent= "center"
-container.style.gridTemplateColumns="10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px" 
-container.style.gridTemplateRows= "10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px "
+container.style.gridTemplateColumns= "10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px 10px "
 
 
+//OneSide number of squares
+let a = 16;
 
 //array of divs
  // MANUAL WAY not in use: const divs = [creatingDivs(), creatingDivs()]
-const divs = (Array(256).fill("creatingDivs()"))
+const divs = (Array(a*a).fill("creatingDivs()"))
 
 //function maker of divs
 function creatingDivs(i){
         const div = document.createElement("div")
             div.id="div"+i;
+            div.style.backgroundColor="#FFF"
             div.onmouseover = function() {mouseOver(i)};
-            div.onmouseout = function() {mouseOff(i)};
+            //div.onmouseout = function() {mouseOff(i)};  IN CASE IF YOU WANT NOT LEAVE TRACE
             div.style.height=10;
             div.style.width=10;
             return div
 }
 
 //turn strings into function calls
-for(let i=0;i<256;i++){
+for(let i=0;i<a*a;i++){
      divs[i]=creatingDivs(i)
     }
 
-
-
 //appendig divs
-console.log(divs)
+
     divs.forEach((divs) => {container.appendChild(divs)})
 
 //changing color Functions
@@ -64,4 +65,17 @@ function mouseOver(i){
 function mouseOff(i){
     document.getElementById("div"+i).style.background = "#FFF"};
 
-    
+//Click en ChangeSize Button
+let asking = 0
+
+document.getElementById("button").addEventListener("click", function(){
+    a=0;
+    asking = prompt("How many square per side do you want?", "16");
+    if(asking==NaN||asking==null){
+        asking = prompt("Please enter a valid positive number", "16");
+    }
+        else {;
+            a=asking;
+            return asking};
+})
+console.log(asking);
