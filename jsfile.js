2352c3c1ff04bff407aafe081961f79570dcb1b7
Bhrqz <josebchrist@hotmail.com>
2020-04-14 13:36:43 -0500

    just one bug to goal

--- jsfile.js
+++ jsfile.js
@@ -1,9 +1,13 @@
 //Setting bigDiv
 const bigDiv = document.querySelector("#bigDiv")
-bigDiv.style.marginLeft= "auto";
-bigDiv.style.marginRight = "auto";
 bigDiv.style.width = "100%";
-bigDiv.style.alignContent = "center";
+
+//ccontainer
+const ccontainer = document.querySelector("#ccontainer")
+ccontainer.style.display="grid";
+ccontainer.style.justifyContent="center"
+
+
 
 //div button CahngeSize
 const buttonChange = document.getElementById("#buttonDiv")
@@ -15,45 +19,50 @@ buttonDiv.style.alingItems = "center"
 
 //button changeSize
 const button = document.querySelector('#button');
-button.style.marginBottom =10;
+button.style.marginBottom =15;
 
 //title 
 const title = document.getElementById("#title");
 title.style.textAlign = "center"
 title.style.marginTop= 30;
 
-
+//OneSide number of squares
+let size = 4;
 
 //div container
 const container = document.querySelector("#container")
 container.style.display= "grid"
-container.style.backgroundColor="gray"
+//container.style.backgroundColor="gray"
+container.style.width=480;  //FOR SOME REASON THERE IS NOT CENTERED WHEN ACTIVATED
+container.style.height=480;
 container.style.justifyContent= "center"
+container.classList.add("container")
+container.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
+container.style.gridTemplateRows= `repeat(${size}, 1fr)`;
 
-container.style.gridTemplateColumns= "repeat(16, 30px)";
 
 
-//OneSide number of squares
-let a = 16;
+
 
 //array of divs
  // MANUAL WAY not in use: const divs = [creatingDivs(), creatingDivs()]
-const divs = (Array(a*a).fill("creatingDivs()"))
+const divs = (Array(size*size).fill("creatingDivs()"))
 
 //function maker of divs
-function creatingDivs(i){
+function creatingDivs(i, size){
         const div = document.createElement("div")
             div.id="div"+i;
             div.style.backgroundColor="#FFF"
             div.onmouseover = function() {mouseOver(i)};
             //div.onmouseout = function() {mouseOff(i)};  IN CASE IF YOU WANT NOT LEAVE TRACE
-            div.style.height=30;
-            div.style.width=30;
+            div.style.height=1/size;
+            div.style.width=1/size;
+            //div.classList.add("square"); Class JUST IN CASE
             return div
 }
 
 //turn strings into function calls
-for(let i=0;i<a*a;i++){
+for(let i=0;i<size*size;i++){
      divs[i]=creatingDivs(i)
     }
 
@@ -70,11 +79,11 @@ function mouseOff(i){
 //Click en ChangeSize Button
 document.getElementById("button").addEventListener("click", gettingNewGrid);
 
-let size=0;
+size=0;
 
 function gettingNewGrid() {
     let asking = prompt("How many squares per size do you want?", 16);
-            if (asking == "" || isNaN(asking)){
+            if (asking == "" || isNaN(asking) || asking===null){
                 alert("Hit Change Size button and make a new canvas");
             } 
             else{divs.forEach((divs) => {container.innerHTML =""});
