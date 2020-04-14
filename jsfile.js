3da5429cebe98453268e4aca56d9e260a681a1ef
Bhrqz <josebchrist@hotmail.com>
2020-04-14 16:40:50 -0500

    bug solved, before background

--- jsfile.js
+++ jsfile.js
@@ -6,6 +6,7 @@ bigDiv.style.width = "100%";
 const ccontainer = document.querySelector("#ccontainer")
 ccontainer.style.display="grid";
 ccontainer.style.justifyContent="center"
+ccontainer.style.backgroundColor="#0F9";
 
 
 
@@ -26,9 +27,6 @@ const title = document.getElementById("#title");
 title.style.textAlign = "center"
 title.style.marginTop= 30;
 
-//OneSide number of squares
-let size = 4;
-
 //div container
 const container = document.querySelector("#container")
 container.style.display= "grid"
@@ -37,68 +35,72 @@ container.style.width=480;  //FOR SOME REASON THERE IS NOT CENTERED WHEN ACTIVAT
 container.style.height=480;
 container.style.justifyContent= "center"
 container.classList.add("container")
-container.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
-container.style.gridTemplateRows= `repeat(${size}, 1fr)`;
+container.style.gridTemplateColumns= `repeat(16, 1fr)`;
+container.style.gridTemplateRows= `repeat(16, 1fr)`;
 
 
+function mainSetting(size){
+        console.log(size);
 
+        container.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows= `repeat(${size}, 1fr)`;
 
+        //array of divs
+        // MANUAL WAY not in use: const divs = [creatingDivs(), creatingDivs()]
+        const divs = (Array(size*size).fill("creatingDivs()"))
 
-//array of divs
- // MANUAL WAY not in use: const divs = [creatingDivs(), creatingDivs()]
-const divs = (Array(size*size).fill("creatingDivs()"))
+        //function maker of divs
+        function creatingDivs(i, size){
+                const div = document.createElement("div")
+                    div.id="div"+i;
+                    div.style.backgroundColor="#FFF"
+                    div.onmouseover = function() {mouseOver(i)};
+                    //div.onmouseout = function() {mouseOff(i)};  IN CASE IF YOU WANT NOT LEAVE TRACE
+                    div.style.height=1/size;
+                    div.style.width=1/size;
+                    //div.classList.add("square"); Class JUST IN CASE
+                    return div
+        }
 
-//function maker of divs
-function creatingDivs(i, size){
-        const div = document.createElement("div")
-            div.id="div"+i;
-            div.style.backgroundColor="#FFF"
-            div.onmouseover = function() {mouseOver(i)};
-            //div.onmouseout = function() {mouseOff(i)};  IN CASE IF YOU WANT NOT LEAVE TRACE
-            div.style.height=1/size;
-            div.style.width=1/size;
-            //div.classList.add("square"); Class JUST IN CASE
-            return div
+        //turn strings into function calls
+        for(let i=0;i<size*size;i++){
+            divs[i]=creatingDivs(i)
+            }
+        console.log(divs)
+        //appendig divs
+
+            divs.forEach((divs) => {container.appendChild(divs)})
+
+        //changing color Functions
+        function mouseOver(i){
+            document.getElementById("div"+i).style.background = "#000"};
+        function mouseOff(i){
+            document.getElementById("div"+i).style.background = "#FFF"};
 }
-
-//turn strings into function calls
-for(let i=0;i<size*size;i++){
-     divs[i]=creatingDivs(i)
-    }
-
-//appendig divs
-
-    divs.forEach((divs) => {container.appendChild(divs)})
-
-//changing color Functions
-function mouseOver(i){
-    document.getElementById("div"+i).style.background = "#000"};
-function mouseOff(i){
-    document.getElementById("div"+i).style.background = "#FFF"};
 
 //Click en ChangeSize Button
 document.getElementById("button").addEventListener("click", gettingNewGrid);
 
-size=0;
-
-function gettingNewGrid() {
-    let asking = prompt("How many squares per size do you want?", 16);
+function gettingNewGrid(divs) {
+        let asking = prompt("How many squares per size do you want?", 16);
             if (asking == "" || isNaN(asking) || asking===null){
                 alert("Hit Change Size button and make a new canvas");
             } 
-            else{divs.forEach((divs) => {container.innerHTML =""});
-                alert("Your new canvas will have "+asking+" squares per side. Nice!")
-                size = asking
+            else{container.innerHTML="";
+                alert("Your new canvas will have "+asking+" squares per side. Nice!");
+                size = asking;
                 console.log(asking);
                 console.log(size);
-                makeNewDivs(size);
-                }}
+                mainSetting(size);
+                }};
 
-function makeNewDivs(size){
+/*function makeNewDivs(size){
     const divs2 = (Array(size*size).fill("creatingDivs()"));
     for(let i=0;i<size*size;i++){
         divs2[i]=creatingDivs(i)
        }
     console.log(divs2);
     divs2.forEach((divs) => {container.appendChild(divs)})
-    }
+    };
+*/
+    mainSetting(16);
